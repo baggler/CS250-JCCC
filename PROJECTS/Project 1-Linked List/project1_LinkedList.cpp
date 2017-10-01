e7e8085283313d92b5ba7c36626c56fd1790be28
Grant H <ghaugland@live.com>
2017-10-01 14:52:50 -0500

    Add files via upload

--- PROJECTS/Project 1-Linked List/project1_LinkedList.cpp
+++ PROJECTS/Project 1-Linked List/project1_LinkedList.cpp
@@ -63,44 +63,48 @@ void LinkedList::PushBack( CustomerData newData )
 
 void LinkedList::PushIndex(CustomerData newData, int index)
 {
+	// Creating a walking pointer and a newNode pointer to hold the data passed in.
 	Node* ptrCurrent = m_ptrFirst;
 	Node* newNode = new Node;
 	newNode->m_data = newData;
-
+	// Counter to keep track of which index we are at while traversing the list
 	int counter = 0;
-
+	//Error checking input
 	if (index < 0 || index > m_itemCount)
 	{
-		throw out_of_range("out of bounds index");
+		throw out_of_range("Index is out of the list's range!");
 	}
-
-	else if (m_itemCount - index == 0)
+	//Check if list is empty, if it is assign first & last ptr's to newNode
+	else if (m_ptrFirst == m_ptrLast)
+	{
+		m_ptrFirst = newNode;
+		m_ptrLast = newNode;
+		return;
+	}
+	//Check to see if the given insert index is zero, if it is call PushFront and pass it the data.
+	else if (index == 0)
 	{
 		PushFront(newData);
+		return;
 	}
-
-	else if (m_itemCount == index)
+	//Check to see if the insert index is at the last node, if it is call PushBack and pass it the data.
+	else if (index == m_itemCount)
 	{
 		PushBack(newData);
+		return;
 	}
-
-	else
+	// Begin traversing the list, once the desired index is reach, ptrCurrent will hold that address.
+	while (counter != index)
 	{
-		while (counter != index)
-		{
-			ptrCurrent = ptrCurrent->m_ptrNext;
-			counter++;
-
-			if (counter == index)
-			{
-				ptrCurrent->m_ptrPrev->m_ptrNext = newNode;
-				newNode->m_ptrPrev = ptrCurrent->m_ptrPrev;
-				newNode->m_ptrNext = ptrCurrent;
-				ptrCurrent->m_ptrPrev = newNode;
-				m_itemCount++;
-			}
-		}
+		ptrCurrent = ptrCurrent->m_ptrNext;
+		counter++;
 	}
+	//Once the index is found, insert the newNode and update all the pointer and itemCount.
+	ptrCurrent->m_ptrPrev->m_ptrNext = newNode;
+	newNode->m_ptrPrev = ptrCurrent->m_ptrPrev;
+	newNode->m_ptrNext = ptrCurrent;
+	ptrCurrent->m_ptrPrev = newNode;
+	m_itemCount++;
 }
 
 void LinkedList::PopFront() noexcept
@@ -143,19 +147,40 @@ void LinkedList::PopBack() noexcept
 
 void LinkedList::RemoveIndex(int index)
 {
-	Node* ptrCurrent = m_ptrFirst;
 	int counter = 0;
+	Node* ptrCurrent = m_ptrFirst;
+	//Check that the index is valid, greater then zero and not bigger then the list size
+	if (index < 0 || index > m_itemCount)
+	{
+		throw out_of_range("Index is out of the list's range");
+	}
+	//Check if the list is empty, if it is just ignore the call and return.
+	else if (m_ptrFirst == nullptr)
+	{
+		return;
+	}
+	//Check if the index is the first node, if it is call PopFront function and return to exit
+	else if (index == 0)
+	{
+		PopFront();
+		return;
+	}
+	//Check if the index is the last node, if it is call PopBack function and return to exit
+	else if (index == m_itemCount)
+	{
+		PopBack();
+		return;
+	}
+	//Index is not first or last, start traversing list and find the index.
 	while (counter != index)
 	{
 		ptrCurrent = ptrCurrent->m_ptrNext;
 		counter++;
-		if (counter == index)
-		{
-			ptrCurrent->m_ptrNext->m_ptrPrev = ptrCurrent->m_ptrPrev;
-			ptrCurrent->m_ptrPrev->m_ptrNext = ptrCurrent->m_ptrNext;
-			m_itemCount--;
-		}
 	}
+	//Index to be removed has been found, reassign the ptrPrev and PtrNext pointers
+	ptrCurrent->m_ptrNext->m_ptrPrev = ptrCurrent->m_ptrPrev;
+	ptrCurrent->m_ptrPrev->m_ptrNext = ptrCurrent->m_ptrNext;
+	m_itemCount--;
 }
 
 CustomerData LinkedList::GetFront()
