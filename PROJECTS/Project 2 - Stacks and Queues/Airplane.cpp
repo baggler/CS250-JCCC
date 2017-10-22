584f4025ab3b35bb49f25c79870a2489f4945da9
Grant H <ghaugland@live.com>
2017-10-22 13:25:42 -0500

    Add files via upload

---
+++ PROJECTS/Project 2 - Stacks and Queues/Airplane.cpp
@@ -0,0 +1,40 @@
+#include "Airplane.hpp"
+
+
+//! Move traveller onto airplane
+/**
+    For the traveller passed in, push it into the airplane's stack.
+
+    @param <Traveller*> traveller
+    @return <void>
+*/
+void Airplane::Board(Traveller* traveller)
+{
+	Travellers.Push(traveller);
+}
+
+//! Pull traveller off airplane
+/**
+    Get the next (top-most) traveller from the airplane stack,
+    remove them from the stack and return the traveller.
+
+    @return <Traveller*>
+*/
+Traveller* Airplane::Disembark()
+{
+	Traveller* topTraveller = Travellers.Top();
+	Travellers.Pop();
+	return topTraveller;
+}
+
+//! Return whether the airplane is empty (no passengers)
+/**
+    If the size of the stack is 0, then the airplane is empty.
+
+    @return <bool>
+*/
+bool Airplane::IsEmpty()
+{
+	return(Travellers.Size() == 0);
+}
+
